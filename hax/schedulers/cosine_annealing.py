de3bf4f3fcc7cc54d278e5c9e1a49d12dc6ebb43
DavidHerreros <david.herreroscalero@gmail.com>
2025-11-17 10:31:31 +0100

    Improved cosine annealing parameters

--- hax/schedulers/cosine_annealing.py
+++ hax/schedulers/cosine_annealing.py
@@ -4,18 +4,18 @@ import optax
 class CosineAnnealingScheduler:
 
     @staticmethod
-    def getScheduler(base_lr, total_steps, warmup_frac=None):
+    def getScheduler(peak_value, total_steps, warmup_frac=None, **kwargs):
         if warmup_frac is None:
             return optax.cosine_decay_schedule(
-                init_value=base_lr,
+                init_value=peak_value,
                 decay_steps=total_steps,
-                alpha=0.0,   # min relative LR; >0 for a non-zero floor
+                alpha=kwargs.pop("alpha", 0.0),   # min relative LR; >0 for a non-zero floor
                 )
         else:
             return optax.warmup_cosine_decay_schedule(
-                init_value=0.0,          # LR at step 0
-                peak_value=base_lr,      # LR at end of warmup
+                init_value=kwargs.pop("init_value", 0.0),          # LR at step 0
+                peak_value=peak_value,      # LR at end of warmup
                 warmup_steps=int(warmup_frac * total_steps),
                 decay_steps=total_steps, # total length (warmup + cosine)
-                end_value=0.0,           # LR at the very end
+                end_value=kwargs.pop("end_value", 0.0),           # LR at the very end
                 )
